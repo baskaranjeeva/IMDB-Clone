d2ddcdc3644d702fb4e8421cf0cc22ae5dc83e7a
baskaranjeeva <baskaran.jeevanandham@gmail.com>
2025-03-28 15:10:40 +0530

    Routing the pages using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import "./App.css";
 import Banner from "./components/Banner";
 import MovieCard from "./components/MovieCard";
 import Navbar from "./components/Navbar";
-Banner;
+import Watchlist from "./components/Watchlist";
+import MoviesRecommendations from "./components/MoviesRecommendations";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <div className="space-y-10 ">
-        <Banner />
-
-        <div className="flex flex-wrap justify-center">
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-          <MovieCard />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner />
+                <div className="flex flex-wrap justify-center gap-8 ml-5 mr-5">
+                  <MovieCard />
+                </div>
+              </>
+            }
+          />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/recommend" element={<MoviesRecommendations />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../IMDB_movie_logo_1.png";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <div className="flex justify-between  items-center bg-teal-800  ">
@@ -7,9 +9,9 @@ function Navbar() {
         <img className="w-[80px]" src={Logo} />
       </div>
       <div className="w-[30rem] flex justify-evenly text-lg font-bold text-white">
-        <a>Movies</a>
-        <a>Watchlist</a>
-        <a>Movies Recommondations</a>
+        <Link to="/">Movies</Link>
+        <Link to="/watchlist">Watchlist</Link>
+        <Link to="/recommend">Movies Recommondations</Link>
       </div>
     </div>
   );
